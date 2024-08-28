23aaa6af9c5e9186fa50895f40ccd01b78297320
AragaoMV <marcosvinisa@icloud.com>
2024-08-28 14:08:57 -0300

    Set spawn position random

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPositionY = 0;
+    private float spawnPositionZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,11 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), spawnPositionY, spawnPositionZ);
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPosition , animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
