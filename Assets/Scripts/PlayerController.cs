dc2221f6e6deadf686fbab3e16f7eecf8000a7a6
AragaoMV <marcosvinisa@icloud.com>
2024-08-28 14:29:57 -0300

    update player and animal range, spawn set as random timer, animals speed changed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     private float speed = 10.0f;
-    private float xRange = 13;
+    private float xRange = 17;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 17;
     private float spawnPositionY = 0;
     private float spawnPositionZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), spawnPositionY, spawnPositionZ);
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex], spawnPosition , animalPrefabs[animalIndex].transform.rotation);
-        }
     }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPositionY, spawnPositionZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+
 }
